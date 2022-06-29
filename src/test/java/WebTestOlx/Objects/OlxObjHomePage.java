103d024a736b33e5c4cd599b62987fac4afe21c0
nigeriobike <n.eugenii2002@gmail.com>
2022-06-29 17:42:53 +0300

    add

--- src/test/java/WebTestOlx/Objects/OlxObjHomePage.java
+++ src/test/java/WebTestOlx/Objects/OlxObjHomePage.java
@@ -3,7 +3,6 @@ package WebTestOlx.Objects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.Test;
 
 public class OlxObjHomePage extends BasePage {
     public OlxObjHomePage(WebDriver driver) {
